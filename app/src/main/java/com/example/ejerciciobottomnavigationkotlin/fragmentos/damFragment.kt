96789ff7e70c060a41246dc8e6dd58d2e82d2c58
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2023-01-20 13:58:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejerciciobottomnavigationkotlin/fragmentos/damFragment.kt
@@ -0,0 +1,20 @@
+package com.example.ejerciciobottomnavigationkotlin.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ejerciciobottomnavigationkotlin.R
+
+class damFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dam, container, false)
+    }
+
+}
