4d7351b5f5c907d6c6d63f1503965dae5ccab53e
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2023-01-23 09:28:57 +0100

    Actualizado

--- app/src/main/java/com/example/ejerciciobottomnavigationkotlin/fragmentos/DamFragment.kt
+++ app/src/main/java/com/example/ejerciciobottomnavigationkotlin/fragmentos/DamFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.ejerciciobottomnavigationkotlin.R
 
-class damFragment : Fragment() {
+class DamFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/ejerciciobottomnavigationkotlin/fragmentos/HomeFragment.kt
+++ app/src/main/java/com/example/ejerciciobottomnavigationkotlin/fragmentos/HomeFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.ejerciciobottomnavigationkotlin.R
 
-class dawFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daw, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
